91163a5e7ad4f6349dea9d1ed51815ff5c518bee
callycodes <61029674+callycodes@users.noreply.github.com>
2020-03-25 19:33:56 +0000

    Netbeans Project Creation
    - Netbeans Project Creation
    - Main Class created

---
+++ MaxFlowProblem/src/main/java/com/callycodes/maxflowproblem/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Name: Callum Bass
+ * Student Number: w1682693
+ * Group: 5SENG001W/2
+ * University of Westminster - Coursework
+ */
+package com.callycodes.maxflowproblem;
+
+/**
+ *
+ * @author Callum Bass <w1682693>
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("Max Flow Problem");
+    }
+    
+}
